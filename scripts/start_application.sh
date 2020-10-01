ea65869e8f6cee88acc4b006141a876a92495e36
u0-blip <chenduona@gmail.com>
2020-10-01 18:57:28 +1000

    add argumnent to create deployment group

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user/www/project/
 source /home/ec2-user/www/project-venv/bin/activate
 mkdir -p /home/ec2-user/www/project/logs/
 # echo yes | DJANGO_SETTINGS_MODULE=peterMusically.settings.staging SECRET_KEY=$DJANGO_SECRET_KEY /home/ec2-user/www/project/manage.py collectstatic
-DJANGO_SETTINGS_MODULE=peterMusically.settings.staging SECRET_KEY=$DJANGO_SECRET_KEY supervisord -c /home/ec2-user/www/project/scripts/supervisor/default.conf 
+DJANGO_SETTINGS_MODULE=peterMusically.settings.staging SECRET_KEY=$ENV_SECRET_KEY supervisord -c /home/ec2-user/www/project/scripts/supervisor/default.conf 
