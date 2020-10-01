752876fab3b509d876404fb64696f8c088402235
u0-blip <chenduona@gmail.com>
2020-10-01 18:50:52 +1000

    add argumnent to create deployment group

--- scripts/ngnix.sh
+++ scripts/ngnix.sh
@@ -13,6 +13,6 @@ cp /home/ec2-user/www/project/scripts/nginx/staging.conf /etc/nginx/sites-availa
 
 ln -s /etc/nginx/sites-available/my-project-host.conf /etc/nginx/sites-enabled/my-project-host.conf
 
-sudo nginx -s reopen
+sudo nginx
 sudo nginx -s reload
 # sudo nginx -s start
