1dc77542ab764a4b555a114c30bb30ec12649a09
u0-blip <chenduona@gmail.com>
2020-10-01 17:58:38 +1000

    add argumnent to create deployment group

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,6 +3,5 @@ cd /home/ec2-user/www/project/
 source /home/ec2-user/www/project-venv/bin/activate
 chmod +x ./manage.py
 . /home/ec2-user/.bashrc
-echo $DJANGO_SECRET_KEY > key3.log
 ./manage.py makemigrations peterMusically
 ./manage.py migrate
