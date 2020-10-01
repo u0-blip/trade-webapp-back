d6f65f8d0cc3ff4dc410dceaa6da08079398d7a5
u0-blip <chenduona@gmail.com>
2020-10-01 18:19:05 +1000

    add argumnent to create deployment group

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,5 +2,6 @@
 cd /home/ec2-user/www/project/
 source /home/ec2-user/www/project-venv/bin/activate
 chmod +x ./manage.py
+. /home/ec2-user/.bashrc
 ./manage.py makemigrations peterMusically
 ./manage.py migrate
